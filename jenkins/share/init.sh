46b1b28e74b069dd64c4260101af0abe54e5d6c2
Sergey Tumanov <tumanser@gmail.com>
2022-07-23 18:15:33 +0300

    Link data from/to host/docker

--- jenkins/share/init.sh
+++ jenkins/share/init.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+
 SHARE_DIR=/root/share
 DATA_DIR=/root/data
 
-mkdir -p $DATA_DIR/jenkins
-ln -s $DATA_DIR/jenkins /root/.jenkins
+
+function link_share {
+    mkdir -p $SHARE_DIR/$1
+    ln -s $SHARE_DIR/$1 $2
+}
+
+function link_data {
+    mkdir -p $DATA_DIR/$1
+    ln -s $DATA_DIR/$1 $2
+}
+
+
+link_data m2 /root/.m2
+link_data jenkins /root/.jenkins
 
 cd /opt &&
 java -jar jenkins.war
